1d135787601013775708c36fc5ea3a9033d0c8db
varsha24122004 <varshakshatriya12345@gmail.com>
2025-08-05 20:03:23 +0530

    second commit

--- src/corejava/Java.java
+++ src/corejava/Java.java
@@ -5,6 +5,7 @@ public class Java {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("helloworld");
+			System.out.println("helloworld2");
 	}
 
 }
